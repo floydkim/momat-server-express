2b22e2db8caef561157f1ab763d59de635783a98
floydkim <r2d2em@empal.com>
2020-07-31 22:09:47 +0900

    prettier 추가

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    singleQuote: true,
+    tabWidth: 4,
+    bracketSpacing: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    printWidth: 90,
+};
--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,17 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:${
 
 const foo = () => {
     const a = 'a';
+    const obj = {
+        a: 'a',
+        b: 2,
+    };
+    const arr = Object.keys(obj);
+    console.log(arr, a);
+    const arr2 = [1, 3, 4];
+
+    const obj2 = { a: 1 };
+
     return {};
 };
+
+foo();
